efb1384fd034107c1e4e903c16c09acab7bc296f
tomasbrezina <tomas.brezinaaa@gmail.com>
2024-02-06 23:09:36 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-green-400">HELLO</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+pinia.use(createPersistedState)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
